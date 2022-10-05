3500b906a9a989d3845f72b874cd5de37845bf2f
oleksandr-vasylenko <32594604+oleksandr-vasylenko@users.noreply.github.com>
2022-10-05 21:28:38 +0000

    Deploying to gh-pages from @ oleksandr-vasylenko/goit-js-hw-09@fec171c51fdd2aff8e8e57d51b7d2c693e48d52a 🚀

--- 01-color-switcher.827a9109.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){timerID=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.backgroundColor=t}),1e3),t.setAttribute("disabled","")})),e.addEventListener("click",(function(){clearInterval(timerID),t.removeAttribute("disabled")}))}();
-//# sourceMappingURL=01-color-switcher.827a9109.js.map
