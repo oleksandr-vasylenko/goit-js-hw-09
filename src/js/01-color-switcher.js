c0fc016054c8c29255a83e78c05225247108deba
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-10-06 00:10:12 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+let bcgColor = '';
+
+const startBtn = document.querySelector('[data-start]');
+const stoptBtn = document.querySelector('[data-stop]');
+
+startBtn.addEventListener('click', onStart);
+
+function onStart() {
+  timerID = setInterval(() => {
+    let bcgColor = getRandomHexColor();
+    document.body.style.backgroundColor = bcgColor;
+  }, 1000);
+  startBtn.setAttribute('disabled', '');
+}
+
+stoptBtn.addEventListener('click', onStop);
+
+function onStop() {
+  clearInterval(timerID);
+  startBtn.removeAttribute('disabled');
+}
