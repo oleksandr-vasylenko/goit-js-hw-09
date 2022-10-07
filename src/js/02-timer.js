ad2d5dc12e1f65341b2602c80222482bf7dc6340
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-10-07 20:47:33 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,6 +2,12 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
 const startBtn = document.querySelector('[data-start]');
+const daysNumber = document.querySelector('[data-days]');
+const hoursNumber = document.querySelector('[data-hours]');
+const minutesNumber = document.querySelector('[data-minutes]');
+const secondsNumber = document.querySelector('[data-seconds]');
+// const dateSelect = document.querySelector('#datetime-picker');
+
 startBtn.setAttribute('disabled', '');
 
 const options = {
@@ -20,13 +26,36 @@ const options = {
     } else {
       startBtn.removeAttribute('disabled', '');
     }
+    let ms = pickedDate - currentDate;
+
+    function convertMs(ms) {
+      // Number of milliseconds per unit of time
+      const second = 1000;
+      const minute = second * 60;
+      const hour = minute * 60;
+      const day = hour * 24;
+
+      // Remaining days
+      const days = Math.floor(ms / day);
+      // Remaining hours
+      const hours = Math.floor((ms % day) / hour);
+      // Remaining minutes
+      const minutes = Math.floor(((ms % day) % hour) / minute);
+      // Remaining seconds
+      const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+      return { days, hours, minutes, seconds };
+    }
+
+    startBtn.addEventListener('click', onStart);
+
+    function onStart() {
+      daysNumber.textContent = convertMs(ms).days;
+      hoursNumber.textContent = convertMs(ms).hours;
+      minutesNumber.textContent = convertMs(ms).minutes;
+      secondsNumber.textContent = convertMs(ms).seconds;
+    }
   },
 };
 
 const dataPicker = new flatpickr('#datetime-picker', options);
-
-startBtn.addEventListener('click', onStart);
-
-function onStart(e) {
-  console.log(e);
-}
