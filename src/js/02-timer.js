f8b2349cfe47b24983284a9b4ef72a96c4806594
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-10-07 17:34:10 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -12,33 +12,6 @@ const options = {
 };
 
 const dataPicker = new flatpickr('#datetime-picker', options);
-console.dir(dataPicker.selectedDates[0]);
+console.log(dataPicker.selectedDates[0]);
 
-// console.log(Date());
-
-// console.log(Date.parse(Date()));
-
-// console.log(Date.parse(options.defaultDate.toString()));
-
-// console.log(Date.parse(Date()) < Date.parse(options.defaultDate.toString()));
-
-// console.dir(dataPicker.selectedDates[0]);
-// console.dir(JSON.stringify(dataPicker.selectedDates[0]));
-
-// const dateSelect = document.querySelector('#datetime-picker');
-// const startBtn = document.querySelector('[data-start]');
-// const daysNumber = document.querySelector('[data-days]');
-// const hoursNumber = document.querySelector('[data-hours]');
-// const minutesNumber = document.querySelector('[data-minutes]');
-// const secondsNumber = document.querySelector('[data-seconds]');
-
-// startBtn.addEventListener('click', onStart);
-// dateSelect.addEventListener('focus', flatpickr);
-
-// function onStart(e) {}
-
-// if (options.defaultDate.toString() >= Date()) {
-//   console.log('you selected date ahead. please expect');
-// } else {
-//   console.log("you can't select date earlier than today");
-// }
+console.log(new Date().getTime());
