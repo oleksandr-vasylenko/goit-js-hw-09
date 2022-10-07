4c09c4d432ff5ef934abb0e70b00b5a3e10f9de6
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-10-07 19:53:27 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,17 +1,25 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
+const startBtn = document.querySelector('[data-start]');
+startBtn.setAttribute('disabled', '');
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.dir(selectedDates[0]);
+    // console.dir(selectedDates[0]);
+    let pickedDate = Date.parse(dataPicker.selectedDates[0]);
+    let currentDate = new Date().getTime();
+
+    if (pickedDate <= currentDate) {
+      window.alert('Please choose a date in the future');
+    } else {
+      startBtn.removeAttribute('disabled', '');
+    }
   },
 };
 
 const dataPicker = new flatpickr('#datetime-picker', options);
-console.log(dataPicker.selectedDates[0]);
-
-console.log(new Date().getTime());
