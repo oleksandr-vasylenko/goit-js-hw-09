cd5a52dfa6d00663090a6be5ceeb6549855685cb
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-10-08 17:05:28 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,13 +6,13 @@ const daysNumber = document.querySelector('[data-days]');
 const hoursNumber = document.querySelector('[data-hours]');
 const minutesNumber = document.querySelector('[data-minutes]');
 const secondsNumber = document.querySelector('[data-seconds]');
-// const dateSelect = document.querySelector('#datetime-picker');
 
 startBtn.setAttribute('disabled', '');
 
 let pickedDate = 0;
 let currentDate = 0;
 let ms = 0;
+let timerId = null;
 
 const options = {
   enableTime: true,
@@ -40,19 +40,14 @@ function datePicker() {
 }
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
@@ -61,8 +56,11 @@ function convertMs(ms) {
 startBtn.addEventListener('click', onStart);
 
 function onStart() {
-  daysNumber.textContent = convertMs(ms).days;
-  hoursNumber.textContent = convertMs(ms).hours;
-  minutesNumber.textContent = convertMs(ms).minutes;
-  secondsNumber.textContent = convertMs(ms).seconds;
+  timerId = setInterval(() => {
+    daysNumber.textContent = convertMs(ms).days;
+    hoursNumber.textContent = convertMs(ms).hours;
+    minutesNumber.textContent = convertMs(ms).minutes;
+    secondsNumber.textContent = convertMs(ms).seconds;
+    console.log(minutesNumber.textContent);
+  }, 1000);
 }
