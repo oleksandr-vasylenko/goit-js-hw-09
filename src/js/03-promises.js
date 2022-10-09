8beccaa3fa065ece4e9cfd6b0247d90a817365b7
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-10-09 19:42:19 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,25 @@
+const delayRef = document.querySelector('[name="delay"]');
+const stepRef = document.querySelector('[name="step"]');
+const amountRef = document.querySelector("[name='amount']");
+const createRef = document.querySelector('button');
+
+let data = {};
+
+delayRef.addEventListener('input', onDelayInput);
+stepRef.addEventListener('input', onStepInput);
+amountRef.addEventListener('input', onAmountInput);
+createRef.addEventListener('click', createPromise);
+
+function onDelayInput(e) {
+  data[e.target.name] = e.target.value;
+}
+
+function onStepInput(e) {
+  data[e.target.name] = e.target.value;
+}
+
+function onAmountInput(e) {}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
